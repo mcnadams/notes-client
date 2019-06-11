78a6513ce9d54becc4e1d4db93d87c64dc904dec
mcnadams <mcneilbo@oregonstate.edu>
2019-06-11 16:43:14 -0700

    prepare for back end

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,9 +1,9 @@
 import { WebAuth } from 'auth0-js';
 
 const auth0 = new WebAuth({
-  domain: 'mcnadams.auth0.com',
-  clientID: 'URjY6vA053cfXESJ0h7V27NzyhPsSFHh',
-  redirectUri: 'http://localhost:7890/callback',
+  domain: process.env.AUTH0_DOMAIN,
+  clientID: process.env.AUTH0_CLIENT,
+  redirectUri: process.env.AUTH0_CALLBACK,
   responseType: 'token id_token',
   scope: 'openid'
 });
