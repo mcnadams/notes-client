f5fed8204c503df204cfd65e64d12c2474015830
mcnadams <mcneilbo@oregonstate.edu>
2019-06-11 17:22:44 -0700

    note notes component container

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import CreateNote from '../container/notes/createNote';
+import AllNotes from '../container/notes/AllNotes';
 
 export default function App() {
-  return <CreateNote />;
+  return (
+  <>
+    <CreateNote />
+    <AllNotes />
+  </>
+  );
 }
